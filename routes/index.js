f1106305a48b148bf31a2f546704f9532f23b23e
BansariPatel96 <46641027+BansariPatel96@users.noreply.github.com>
2019-06-16 18:53:16 -0400

    Different 5 pages of personal discription  created for portfolio

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', (req, res, next) => {
+  res.render('home', { title: 'Home' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About Me' });
+});
+
+router.get('/projects', (req, res, next) => {
+  res.render('projects', { title: 'Projects' });
+});
+
+router.get('/services', (req, res, next) => {
+  res.render('services', { title: 'Services' });
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'Contact Me' });
+});
 module.exports = router;
